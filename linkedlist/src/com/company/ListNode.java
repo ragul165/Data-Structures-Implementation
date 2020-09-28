902491e332a879c5cca3cf5a589bc004cd6b0681
Rahul@mail.com <53390666+ragul165@users.noreply.github.com>
2020-09-28 23:08:15 +0530

    first-commit

---
+++ linkedlist/src/com/company/ListNode.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.lang.*;
+
+public class ListNode {
+    private int data;
+    private ListNode next;
+    public ListNode(int data){
+        this.data=data;
+    }
+    public void setData(int data){
+        this.data=data;
+    }
+    public int getData(){
+        return data;
+    }
+    public void setNext(ListNode next){
+        this.next=next;
+    }
+    public ListNode getNext(){
+        return this.next;
+    }
+
+}
+
+
